31f36b118e2db084eda179f06d048ab37ea195c4
andtan <jornandretangen@gmail.com>
2013-02-04 13:42:27 +0100

    init

--- index.js
+++ index.js
@@ -1,12 +1,74 @@
 
-module.exports = function( name ) {
-  return function( obj ) {
-    return obj[ name ]
+
+function match( form ) {
+  var form
+  
+  form = createForm( toArray(arguments) )
+  
+  return function() {
+    var args, i, seq, pattern, then
+
+    args = toArray( arguments )
+
+    for( i = 0; i < form.length; i++ ) {
+
+      seq = form[ i ]
+
+      pattern = seq[ 0 ]
+      then = seq[ 1 ]
+
+      if( isMatch(pattern, args) ) {
+        return typeof then === 'function' ? then.apply( this, args ) : then
+      }
+
+    }
+
+    throw new TypeError( 'no match found for' )
   }
 }
 
-module.exports.backbone = function( name ) {
-  return function( obj ) {
-    return obj.get( name )
+
+function isMatch( pattern, args ) {
+  var i, match, cand, matches
+
+  matches = 0
+
+  for( i = 0; i < pattern.length; i++ ) {
+    match = pattern[ i ]
+    cand = args[ i ]
+
+    if( match == Number && typeof cand == 'number' ) matches++
+    if( match == String && typeof cand == 'string' ) matches++
+    if( typeof match == 'function' && cand instanceof match ) matches++
+    if( match === cand ) matches++
+
   }
+
+  return matches === pattern.length
 }
+
+
+function createForm( array ) {
+  var form, i, item, seq
+
+  form = []
+
+  for (i = 0; i < array.length; i++) {
+    item = array[ i ]
+    if( i === 0 || !(i % 2) ) {
+      seq = []
+    } else {
+      form.push( seq )  
+    }
+    seq.push( item )
+  }
+
+  return form
+}
+
+
+function toArray( args ) {
+  return Array.prototype.slice.call( args, 0 )
+}
+
+module.exports = match
---
+++ tests/match.js
@@ -0,0 +1,55 @@
+
+var match   = require( '../index' ),
+    assert  = require( 'assert' )
+
+
+
+describe('func-match', function() {
+
+
+
+  it('should match values or types and return the match', function() {
+
+    var nrmatch = match( [1],       'one',
+                         [2],       'two',
+                         [Number],  'number' )
+
+    assert.equal( nrmatch(1), 'one' )
+    assert.equal( nrmatch(2), 'two' )
+    assert.equal( nrmatch(8), 'number' )
+
+  })
+
+
+  it('should lazily invoke and return the returned value if the match is a function', function() {
+
+    var lazy = match( [String], function( string ) { return "string" } )
+
+    assert.equal( lazy('s'), 'string' )
+
+  })
+
+  it('should work with constructed native types and literals', function() {
+
+    var isString = match( [String], function() { return true } )
+
+    assert.equal( isString(new String("string")), true )
+    assert.equal( isString("string"), true )
+
+  })
+
+  it('should invoke the matching function with the parameters passed', function() {
+
+    var concat = match([String, String], function( a, b ) { 
+                         return a + b 
+                       },
+                       [String, Number], function( s, n ) { 
+                         return s + ':' + n 
+                       })
+
+    assert.equal( concat('foo', 'bar'), 'foobar' )
+    assert.equal( concat('foo', 12), 'foo:12' )
+
+  })
+
+})
